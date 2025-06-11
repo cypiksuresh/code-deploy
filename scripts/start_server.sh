cf46a4e63ebe1ed1b0bc1cb34dbdf25e7a6b2f30
sohanyadav <sohanyadavdevops@gmail.com>
2025-06-11 17:15:02 +0530

    update new saa

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,9 @@
 
 echo "PATH=$PATH"
 
-mkdir -p /home/ubuntu/test-deploy
+# Make sure directory exists and owned by ubuntu
+sudo mkdir -p /home/ubuntu/test-deploy
+sudo chown ubuntu:ubuntu /home/ubuntu/test-deploy
 
+# Write test log
 echo "✅ Hello from CodeDeploy at $(date)" > /home/ubuntu/test-deploy/deploy_log.txt
